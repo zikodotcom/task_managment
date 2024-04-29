77f16470fa1c0b51f8d40e085e3808f8229fdaae
zikodotcom <foot.ball.american123@gmail.com>
2024-04-29 10:08:18 +0200

    First configuration

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Home from '@/views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
+
+export default router
